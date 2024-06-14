8bf3239d920356b371c7b7c1f87809897c343faa
fortinb <fortinb.perso@gmail.com>
2024-06-14 16:20:59 -0400

    tests

--- proj/hypothenus.common.tests/src/main/java/com/isoceles/hypothenus/tests/http/HttpUtils.java
+++ proj/hypothenus.common.tests/src/main/java/com/isoceles/hypothenus/tests/http/HttpUtils.java
@@ -2,11 +2,19 @@ package com.isoceles.hypothenus.tests.http;
 
 import java.net.MalformedURLException;
 import java.net.URI;
+import java.util.UUID;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.util.UriComponentsBuilder;
 
-public class HttpUtils {
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.isoceles.hypothenus.tests.security.SecurityUtils;
+
+public class HttpUtils<T> {
+	public static final String X_TrackingNumber = "x-tracking-number";
 	
 	public static final URI createURL(URI uri, int port, MultiValueMap<String, String> queryStringParameters) throws MalformedURLException {
 		URI target = UriComponentsBuilder.newInstance()
@@ -19,5 +27,17 @@ public class HttpUtils {
 			.toUri();
 		
 		return target;
+	}
+	
+	public static final <T> HttpEntity<T> createHttpEntity(String role, String admin, T body) throws JsonProcessingException {
+		HttpHeaders httpHeaders = SecurityUtils.getHeaders(role, admin);
+		
+		httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+		
+		httpHeaders.add(X_TrackingNumber, UUID.randomUUID().toString());
+		
+		HttpEntity<T> httpEntity = new HttpEntity<T>(body, httpHeaders);
+		
+		return httpEntity;
 	}
 }
--- proj/hypothenus.common.tests/src/main/java/com/isoceles/hypothenus/tests/security/SecurityUtils.java
+++ proj/hypothenus.common.tests/src/main/java/com/isoceles/hypothenus/tests/security/SecurityUtils.java
@@ -12,13 +12,13 @@ public class SecurityUtils {
 
 	public static final String X_Authorization = "x-authorization";
 	public static final String X_Credentials = "x-credentials";
-	public static final String X_TrackingNumber = "x-tracking-number";
+
 
 	public static HttpHeaders getHeaders(String role, String username) throws JsonProcessingException {
 		
 		HttpHeaders headers = new HttpHeaders();
 		headers.add(X_Credentials, username);
-		headers.add(X_TrackingNumber, UUID.randomUUID().toString());
+		
 		
 		AuthorizationHeader authorization = new AuthorizationHeader();
 		authorization.addRole(role);
