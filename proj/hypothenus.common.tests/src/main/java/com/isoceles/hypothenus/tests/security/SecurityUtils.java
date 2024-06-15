4143c4dd41e4e7f20e2fc4ba47a6cae69f52cffc
fortinb <fortinb.perso@gmail.com>
2024-06-15 08:24:33 -0400

    commit

--- proj/hypothenus.common.tests/src/main/java/com/isoceles/hypothenus/tests/security/SecurityUtils.java
+++ proj/hypothenus.common.tests/src/main/java/com/isoceles/hypothenus/tests/security/SecurityUtils.java
@@ -1,31 +1,28 @@
 package com.isoceles.hypothenus.tests.security;
 
-import java.util.UUID;
-
 import org.springframework.http.HttpHeaders;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-
 public class SecurityUtils {
 
 	public static final String X_Authorization = "x-authorization";
 	public static final String X_Credentials = "x-credentials";
-
-
+	
+	private static ObjectMapper objectMapper = new ObjectMapper();
+	
 	public static HttpHeaders getHeaders(String role, String username) throws JsonProcessingException {
-		
+
 		HttpHeaders headers = new HttpHeaders();
 		headers.add(X_Credentials, username);
-		
-		
+
 		AuthorizationHeader authorization = new AuthorizationHeader();
 		authorization.addRole(role);
-		ObjectMapper mapper = new ObjectMapper();
-		headers.add(X_Authorization, mapper.writeValueAsString(authorization));
 		
+		headers.add(X_Authorization, objectMapper.writeValueAsString(authorization));
+ 
 		return headers;
 	}
-	
+
 }
