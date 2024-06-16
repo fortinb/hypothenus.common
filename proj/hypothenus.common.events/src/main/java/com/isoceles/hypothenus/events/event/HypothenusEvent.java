3b98983411bd2652cbf84587fe64a10acd656b2e
fortinb <fortinb.perso@gmail.com>
2024-06-16 08:40:12 -0400

    events

--- proj/hypothenus.common.events/src/main/java/com/isoceles/hypothenus/events/event/HypothenusEvent.java
+++ proj/hypothenus.common.events/src/main/java/com/isoceles/hypothenus/events/event/HypothenusEvent.java
@@ -2,12 +2,20 @@ package com.isoceles.hypothenus.events.event;
 
 import org.springframework.context.ApplicationEvent;
 
+import lombok.Getter;
+
+@Getter
 public class HypothenusEvent<T> extends ApplicationEvent {
 
 	private static final long serialVersionUID = 1L;
 
-	public HypothenusEvent(Object source) {
-		super(source);
-	}
+	private T entity;
+	private OperationEnum operation;
 
+	public HypothenusEvent(Object source, T entity, OperationEnum operation) {
+		super(source);
+
+		this.entity = entity;
+		this.operation = operation;
+	}
 }
---
+++ proj/hypothenus.common.events/src/main/java/com/isoceles/hypothenus/events/event/OperationEnum.java
@@ -0,0 +1,11 @@
+package com.isoceles.hypothenus.events.event;
+
+public enum OperationEnum {
+	add,
+	update,
+	delete,
+	register,
+	cancel,
+	subscription,
+	referal
+}
